872616530e7872fdfcb1ac232b3f683fe4d1ea0a
Marcelo Gachet <marcelo@growdnd.com>
2022-03-01 08:04:08 -0800

    Added query string capabilities

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,18 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl =  document.querySelector("#repo-name");
+
+var getRepoName = function () {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if (repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    }
+    else {
+        document.location.replace("./index.html");
+    }
+}
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -11,11 +24,11 @@ var getRepoIssues = function (repo) {
             });
             // check if api has paginated issues
             if (response.headers.get("Link")) {
-              displayWarning(repo);
+                displayWarning(repo);
             }
         }
         else {
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
 };
@@ -56,16 +69,16 @@ var displayIssues = function (issues) {
     }
 };
 
-var displayWarning = function(repo) {
+var displayWarning = function (repo) {
     // add text to warning container
     limitWarningEl.textContent = "To see more than 30 issues, visit ";
     var linkEl = document.createElement("a");
-  linkEl.textContent = "See More Issues on GitHub.com";
-  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
-  linkEl.setAttribute("target", "_blank");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
 
-  // append to warning container
-  limitWarningEl.appendChild(linkEl);
-  };
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
 
-getRepoIssues("facebook/react");
+getRepoName();
